0d9097972a7a295e9e08f44e358bc3ce3bed26a0
Ryan Davis <epikit@gmail.com>
2014-10-20 18:51:57 -0500

    - created tannus.db.tandb package folder - created tannus.db.tandb.Database class file -NOTE: tannus.db.tandb.Database class is currently empty

--- tannus/db/tandb/Database.hx
+++ tannus/db/tandb/Database.hx
@@ -2,3 +2,11 @@ package tannus.db.tandb;
 
 import tannus.core.EventDispatcher;
 import tannus.core.Object;
+
+import tannus.utils.PathTools;
+import tannus.utils.File;
+import tannus.utils.Folder;
+
+class Database {
+	
+}
