8b99266b3f9d13b92fc7ed6b01e2bb5d5dc9b1b4
Ryan Davis <epikit@gmail.com>
2014-12-30 06:06:11 -0600

    modified to be a demo for the new graphics api

--- pages/Home.hx
+++ pages/Home.hx
@@ -5,8 +5,18 @@ import tannus.core.Route;
 import tannus.io.Ptr;
 import tannus.dom.Element;
 import tannus.math.Random;
+import tannus.geom.Point;
+
+import tannus.graphics.Canvas;
+import tannus.graphics.CanvasProgram;
+
+import tannus.events.MouseEvent;
 
 class Home extends Page {
+	public var canvas : Canvas;
+
+	public var current_program : CanvasProgram;
+
 	public function new(r:Route):Void {
 		super(r);
 		
@@ -14,13 +24,113 @@ class Home extends Page {
 	}
 
 	public function init():Void {
-		var r:Random = new Random();
+		//- get a reference to canvas we'll be manipulating
+		var canvas_el:Element = new Element('#stage');
 
-		var rand = Ptr.dual((new Random()), r);
+		//- create a Canvas object from it
+		this.canvas = new Canvas(cast canvas_el.element);
 
-		for (i in 0...10) {
-			trace( rand );
-		}
+		//- set it's dimensions
+		canvas.width = 320;
+		canvas.height = 240;
+		
+		//- Get a reference to the drawing context
+		var c = canvas.c;
+
+		toolMenu();
+	}
+
+	/**
+	  * Initialize the tool-menu
+	  */
+	public function toolMenu():Void {
+		this.current_program = canvas.startProgram();
+
+		var linecol_input = new Element('#line-color-input');
+		var linewidth_input = new Element('#line-width-input');
+
+		linewidth_input.val = '1';
+		linecol_input.val = 'black';
+
+		/*
+		 == Program-Buttons ==
+		 */
+
+		//- "New Program" button
+		var new_prog = new Element('#new-program');
+
+		//- "Run Program" button
+		var run_prog = new Element('#run-program');
+		
+		//- When the 'run-program' button
+		run_prog.on('click', function(e : Dynamic):Void {
+			
+			canvas.clear();
+			current_program.execute();
+
+		});
+
+		initEvents( linecol_input, linewidth_input );
+	}
+	
+	/**
+	  * Initialize all event-handlers on the stage
+	  */
+
+	public function initEvents(lci:Element, lwi:Element):Void {
+		var c = canvas.c;
+
+		//- last-known position
+		var lastPos:Null<Point> = null;
+
+		//- whether the mouse is currently clicked on the canvas
+		var focused:Bool = false;
+		
+		//- when the mouse-button is pushed down on the canvas
+		canvas.on('mousedown', function(e : MouseEvent):Void {
+			focused = true;
+		});
+		
+
+		//- when the mouse-button is released
+		canvas.on('mouseup', function(e : MouseEvent):Void {
+			focused = false;
+			lastPos = null;
+		});
+		
+		//- when the cursor moves outside the bounds of the canvas
+		canvas.on('mouseleave', function(e : MouseEvent):Void {
+			lastPos = null;
+		});
+
+		//- When the mouse moves inside the canvas
+		canvas.on('mousemove', function(e : MouseEvent):Void {
+			if ( focused ) {
+				var pos = e.pos;
+				trace( e );
+
+				//c.beginPath();
+				//c.circle(pos.x, pos.y, 4);
+				//c.closePath();
+				//c.stroke();
+
+				if (lastPos != null) {
+					//- Draw a line from the last-known cursor-position to the current one
+					current_program.beginPath();
+
+					current_program.strokeStyle = lci.val;
+					current_program.lineWidth = Std.parseFloat( lwi.val );
+
+					current_program.moveTo(lastPos.x, lastPos.y);
+					current_program.lineTo(pos.x, pos.y);
+					current_program.stroke();
+					current_program.closePath();
+				}
+
+				lastPos = pos;
+			}
+		});
+		
 	}
 
 }
