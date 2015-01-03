7d5211d155d1508189a2b350c408e32e9c2bf125
Ryan Davis <epikit@gmail.com>
2015-01-03 01:12:35 -0600

    reimplemented 'tannus.geom.Point' as an abstract around Array<Float>

--- tannus/geom/Point.hx
+++ tannus/geom/Point.hx
@@ -2,110 +2,149 @@ package tannus.geom;
 
 import tannus.geom.Rectangle;
 
-@forward(
-	x,
-	y,
-	z,
-	toString
-)
-abstract Point (CPoint) from CPoint to CPoint {
+abstract Point (Array <Float>) from Array<Float> {
 	public inline function new(?x:Float=0, ?y:Float=0, ?z:Float=0):Void {
-		this = new CPoint(x, y, z);
-	}
-
-	public inline function clone():Point {
-		return (cast this.clone());
+		this = [x, y, z];
 	}
 	
 	/**
-	  * Tests whether [this] Point "equals" [other] Point
+	  * Creates and returns a copy of [this] Point
 	  */
-	@:op(A == B)
-	public inline function equals(other : Point):Bool {
-		return (this.equals(cast other));
+	public inline function clone():Point {
+		return (new Point(self.x, self.y, self.z));
+	}
+
+/*
+ === Mathematical Operators
+*/
+	/**
+	  * Take an expression and applies it for the [x], [y], and [z] fields
+	  */
+	private inline function apply(other:Point, f:Float->Float->Float):Point {
+		return (cast [for (i in 0...3) f(this[i], other.nums[i])]);
+	}
+
+	private function checkAll(other:Point, f:Float->Float->Bool):Bool {
+		for (i in 0...3) {
+			var one:Float = (this[ i ]);
+			var two:Float = (other.nums[ i ]);
+
+			if (!f(one, two)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	/**
-	  * Creates and returns a new Point which is the "sum" of [this] and [other]
+	  * Creates and returns a new Point which is the 'sum' of [this] and [other]
 	  */
 	@:op(A + B)
 	public inline function sum(other : Point):Point {
-		return new Point(
-			(this.x + other.x),
-			(this.y + other.y),
-			(this.z + other.z)
-		);
+		return apply(other, (function(a, b) return (a + b)));
 	}
-
-	/**
-	  * Create and returns a new Point which is the "difference" between [this] and [other]
-	  */
-	@:op(A - B)
-	public inline function difference(other : Point):Point {
-		return new Point(
-			(this.x - other.y),
-			(this.y - other.y),
-			(this.z - other.z)
-		);
-	}
-
-	/**
-	  * Creates and returns a new Point which is the "product" of [this] and [other]
-	  */
-	public inline function product(other : Point):Point {
-		return new Point(
-			(this.x * other.x),
-			(this.y * other.y),
-			(this.z * other.z)
-		);
-	}
-}
-
-class CPoint {
-
-	//- the "x" coordinate
-	public var x:Float;
-
-	//- the "y" coordinate
-	public var y:Float;
-
-	//- the "z" coordinate
-	public var z:Float;
 	
+	@:op(A - B)
+	public inline function dif(other : Point):Point {
+		return apply(other, (function(a, b) return (a - b)));
+	}
+
+	@:op(A * B)
+	public inline function product(other : Point):Point {
+		return apply(other, (function(a, b) return (a * b)));
+	}
+
+	@:op(A / B)
+	public inline function quotient(other : Point):Point {
+		return apply(other, (function(a, b) return (a / b)));
+	}
+
+/*
+ === Comparison Operators ===
+*/
+
+	@:op(A == B)
+	public inline function equals(other : Point):Bool {
+		return (checkAll(other, (function(a, b) return (a == b))));
+	}
+
+	@:op(A != B)
+	public inline function nequals(other : Point):Bool {
+		return (checkAll(other, (function(a, b) return (a != b))));
+	}
+
+	@:op(A > B)
+	public inline function greaterThan(other : Point):Bool {
+		return (self.magnitude > other.magnitude);
+	}
+
+	@:op(A < B)
+	public inline function lessThan(other : Point):Bool {
+		return (self.magnitude < other.magnitude);
+	}
+
 	/**
-	  * Constructor
+	  * internal reference to [this]
 	  */
-	public function new(?x:Float=0, ?y:Float=0, ?z:Float=0):Void {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+	private var self(get, never):Point;
+	private inline function get_self():Point {
+		return (cast this);
 	}
 
-	public function clone():CPoint {
-		return new Point(this.x, this.y, this.z);
+	/**
+	  * reference to [this] as an Array
+	  */
+	public var nums(get, set):Array<Float>;
+	private inline function get_nums():Array<Float> {
+		return (this);
+	}
+	private inline function set_nums(nms : Array<Float>):Array<Float> {
+		this[0] = nms[0];
+		this[1] = nms[1];
+		this[2] = nms[2];
+		
+		return (this);
 	}
 
-	public function equals(other:CPoint):Bool {
-		return (
-			(this.x == other.x) &&
-			(this.y == other.y) &&
-			(this.z == other.z)
-		);
+	/**
+	  * the distance of [this] Point from [0]
+	  */
+	public var magnitude(get, never):Float;
+	private inline function get_magnitude():Float {
+		return (Math.sqrt((self.x * self.x) + (self.y * self.y)));
 	}
 
-	public function relativeTo(other:CPoint):CPoint {
-		return new CPoint(
-			(other.x - this.x),
-			(other.y - this.y),
-			(other.z - this.z)
-		);
+	/**
+	  * "x" field
+	  */
+	public var x(get, set):Float;
+	private inline function get_x():Float {
+		return (this[0]);
+	}
+	private inline function set_x(nx : Float):Float {
+		return (this[0] = nx);
 	}
 
-	public function offsetFactorOf(rect:Rectangle):CPoint {
-		return rect.relateTo(this);
+	/**
+	  * 'y' field
+	  */
+	public var y(get, set):Float;
+	private inline function get_y():Float {
+		return (this[1]);
+	}
+	private inline function set_y(ny : Float):Float {
+		return (this[1] = ny);
 	}
 
-	public function toString():String {
-		return 'Point($x, $y, $z)';
+	/**
+	  * 'z' field
+	  */
+	public var z(get, set):Float;
+	private inline function get_z():Float {
+		return (this[2]);
+	}
+	private inline function set_z(nz : Float):Float {
+		return (this[2] = nz);
 	}
 }
