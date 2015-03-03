7d27c79173946359126d1040f6ac2dbb36ed682e
Ryan Davis <epikit@gmail.com>
2015-03-02 23:49:56 -0600

    Added "remove" method to tannus.utils.HashWrap class

--- tannus/utils/HashWrap.hx
+++ tannus/utils/HashWrap.hx
@@ -5,11 +5,17 @@ abstract HashWrap (Dynamic) from Dynamic {
         this = o;
     }
     
+    /**
+      * Internal reference to [this] as a HashWrap
+      */
     private var self(get, never):HashWrap;
     private inline function get_self():HashWrap {
         return (cast this);
     }
     
+    /**
+      * A List of all Keys of [this] Hash
+      */
     public function keys():List<String> {
         var names = Reflect.fields( this );
         var keyList:List<String> = new List();
@@ -19,6 +25,10 @@ abstract HashWrap (Dynamic) from Dynamic {
         return keyList;
     }
 
+
+    /**
+      * Iterator of all Propertys of [this] Hash
+      */
     public function iterator():Iterator<Property> {
         var keys = self.keys();
         
@@ -36,21 +46,40 @@ abstract HashWrap (Dynamic) from Dynamic {
         };
     }
     
+    /**
+      * Determines whether [key] is a field of [this] Hash
+      */
     public inline function exists(key : String):Bool {
         return (Reflect.getProperty(this, key) != null);
     }
     
+    /**
+      * Retrieves the value [key] from [this] HashWrap
+      */
     @:arrayAccess
     public inline function get <T> (key : String):Null<T> {
         return (Reflect.getProperty(this, key));
     }
     
+    /**
+      * Assigns [this]'s field [key] the value [value]
+      */
     @:arrayAccess
     public inline function set <T> (key:String, value:T):T {
         return untyped (Reflect.setProperty(this, key, value));
     }
+
+    /**
+      * Removes field [key] from [this] Hash
+      */
+    public inline function remove(key : String):Void {
+	Reflect.deleteField(this, key);
+    }
 }
 
+/**
+  * Type Definition for a Property of a Hash
+  */
 private typedef Property = {
 	name:String,
 	value:Dynamic
