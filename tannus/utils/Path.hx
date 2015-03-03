5bc95c19d89c1f820dc0c4a6088caeac06f06cb2
Ryan Davis <epikit@gmail.com>
2015-03-03 17:45:25 -0600

    added "segments", "stripped", and "normalize" fields/methods to tannus.utils.Path class

--- tannus/utils/Path.hx
+++ tannus/utils/Path.hx
@@ -1,27 +1,71 @@
 package tannus.utils;
 
+/**
+  * Abstract class around String, which adds some useful/convenient methods/fields
+  */
 using haxe.io.Path;
 abstract Path (String) from String to String {
 	
 	public inline function new(str:String):Void {
 		this = str;
 	}
+
+	/**
+	  * Reference to [this] as a Path
+	  */
+	public var self(get, never):Path;
+	private inline function get_self():Path {
+		return (cast this);
+	}
 	
-	//- Reference to the directory-name of the file
+	/**
+	  * Reference to the directory-name of the file
+	  */
 	public var directory(get, never):Null<String>;
 	private inline function get_directory():Null<String> {
 		return this.directory();
 	}
 	
-	//- Reference to the extension-name of the file
+	/**
+	  * Reference to the extension-name of the file
+	  */
 	public var extension(get, never):Null<String>;
 	private inline function get_extension():Null<String> {
 		return this.extension();
 	}
 	
-	//- Reference to the base-name of the file, without directory-name or extension-name
+	/**
+	  * Reference to the base-name of the file, without directory-name or extension-name
+	  */
 	public var basename(get, never):Null<String>;
 	private inline function get_basename():Null<String> {
 		return (this.withoutDirectory().withoutExtension());
+	}
+
+	/**
+	  * The URI without any trailing slashes
+	  */
+	public var stripped(get, never):Path;
+	private inline function get_stripped():Path {
+		return cast (this.removeTrailingSlashes());
+	}
+
+	/**
+	  * Get an Array of URI-Segments, separated by the standard path-separator for the current platform
+	  */
+	public var segments(get, never):Array<String>;
+	private inline function get_segments():Array<String> {
+		var n:String = (stripped);
+		var bits = n.split( '/' );
+		return bits;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Normalized version of [this] URI
+	  */
+	public inline function normalize():Path {
+		return cast this.normalize();
 	}
 }
