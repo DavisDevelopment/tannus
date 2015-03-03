28a78860c0da36bcf36cb39fc2805b04959bef13
Ryan Davis <epikit@gmail.com>
2015-03-03 04:29:10 -0600

    Additions to tannus.dom.widgets.TextInput class:  + 'validator' field, allowing the developer to specify a regular    expression that the Input's value must match  + 'valid' field - computed property which checks whether the Input's  value currently passes when tested with it's validator  + 'placeholder' field - gets/sets the underlying Element's  "placeholder" attribute

--- tannus/dom/widgets/TextInput.hx
+++ tannus/dom/widgets/TextInput.hx
@@ -11,18 +11,79 @@ import tannus.events.MouseEvent;
 import tannus.io.Signal;
 import tannus.io.Byte;
 
+/* Utility Imports */
+import tannus.utils.RegEx;
+
 /* Stream Imports */
 import tannus.inputs.KeystrokeStream;
 
 class TextInput extends BaseWidget {
-/* == Standard Instance Fields == */
-	public var keyStream : KeystrokeStream;
-
+	/* Constructor Function */
 	public function new():Void {
 		super();
 		
-		this.el = new Element('<input type="text"></input>');
+		el = new Element('<input type="text"></input>');
 		
-		this.keyStream = new KeystrokeStream( el );
+		keyStream = new KeystrokeStream( el );
 	}
+
+/* == Computed Instance Fields == */
+
+	/**
+	  * Placeholder for [this] TextInput
+	  */
+	public var placeholder(get, set):String;
+	private inline function get_placeholder():String {
+		return (el['placeholder']);
+	}
+	private inline function set_placeholder(nph : String):String {
+		el['placeholder'] = nph;
+		return placeholder;
+	}
+
+	/**
+	  * Regular Expression [this]'s value must adhere to
+	  */
+	public var validator(get, set):Null<RegEx>;
+	private inline function get_validator():Null<RegEx> {
+		return (cast el.hash['__validator']);
+	}
+	private inline function set_validator(nv : Null<RegEx>):Null<RegEx> {
+		el.hash['__validator'] = nv;
+		return validator;
+	}
+
+	/**
+	  * Whether [this] Input's value adheres to any/all constraints placed on it
+	  */
+	public var valid(get, never):Bool;
+	private function get_valid():Bool {
+		//- get reference to [this] Input's validator
+		var val:Null<RegEx> = validator;
+
+		//- if [this] has no validator
+		if (val == null) {
+			return true;
+		}
+
+		//- if [this] has a validator
+		else {
+			//- to ensure type-safety, wrap the check in a try-catch statement
+			try {
+				//- return the result of the validator's "test" method, invoked with [this] Element's value as it's argument
+				return val.test( el.val );
+
+			} catch (err : Dynamic) {
+				//- Default to.. true?
+				return true;
+			}
+		}
+	}
+
+/* == Standard Instance Fields == */
+
+	/**
+	  * ByteStream of KeyStrokes on [this] TextInput
+	  */
+	public var keyStream : KeystrokeStream;
 }
