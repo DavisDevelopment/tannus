2e39c251ed0955ba1c9068a01da519076877e6c6
Ryan Davis <epikit@gmail.com>
2015-03-06 20:59:26 -0600

    Setter was claiming to return stuff, and yet all of the methods it has are Void-Functions.  Haxe did not like this at all

--- tannus/utils/Setter.hx
+++ tannus/utils/Setter.hx
@@ -3,19 +3,18 @@ package tannus.utils;
 import haxe.macro.Expr;
 
 
-abstract Setter <T> (T -> T) from (T -> T) to (T -> T) {
-
-	public inline function new(f : T -> T):Void {
+abstract Setter <T> (T -> Void) from (T -> Void) to (T -> Void) {
+	public inline function new(f : T -> Void):Void {
 		this = f;
 	}
 
 	public inline function set(value : T):Void {
-		return (this(value));
+		(this(value));
 	}
 
 	public static macro function create <T> ( e ):ExprOf<Setter<T>> {
 		return macro new tannus.utils.Setter(function(value) {
-			return ($e = value);
+			($e = value);
 		});
 	}
 }
