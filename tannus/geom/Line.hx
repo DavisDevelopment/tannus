3068237faf380538116b59e5b8eb6b4dba1a4e3e
Ryan Davis <epikit@gmail.com>
2015-03-11 03:07:54 -0500

    Properly documented the Line class

--- tannus/geom/Line.hx
+++ tannus/geom/Line.hx
@@ -4,15 +4,29 @@ import tannus.math.TMath;
 import tannus.geom.Point;
 import tannus.geom.Rectangle;
 
+import tannus.math.Percent;
+
+/**
+  * class meant to represent a line in three-dimensional space
+  */
 class Line {
+	//- The starting position of the Line
 	public var start:Point;
+
+	//- The final position of the Line
 	public var end:Point;
 
+	/* Constructor Function */
 	public function new(start:Point, end:Point):Void {
 		this.start = start;
 		this.end = end;
 	}
 
+/* === Instance Methods === */
+
+	/**
+	  * Calculates the containing Rectangle of [this] Line
+	  */
 	public function rect():Rectangle {
 		var x:Float = Math.min(start.x, end.x);
 		var y:Float = Math.min(start.y, end.y);
