d070e847a833b014c69a1607ff2c3506ef94a8d7
Ryan Davis <epikit@gmail.com>
2015-03-14 00:57:13 -0500

    Changes to tannus.io.Range: 	added 'min' and 'max' fields, aliases to 'one' and 'two' 	added static instance method 'iterator' to Range<Int> 	added static instance method 'rand' to Range<Int>

--- tannus/io/Range.hx
+++ tannus/io/Range.hx
@@ -1,5 +1,7 @@
 package tannus.io;
 
+import tannus.math.Random;
+
 /**
   * Abstract class to represent a "range" between two values
   */
@@ -20,6 +22,17 @@ abstract Range <T> (Array<T>) from Array<T> to Array<T> {
 	private inline function set_one(v : T):T {
 		return (this[0] = v);
 	}
+	
+	/**
+	  * Alias to 'one'
+	  */
+	public var min(get, set) : T;
+	private inline function get_min():T {
+		return (this[0]);
+	}
+	private inline function set_min(v : T):T {
+		return (this[0] = v);
+	}
 
 	/**
 	  * The second element in [this] Range
@@ -31,13 +44,42 @@ abstract Range <T> (Array<T>) from Array<T> to Array<T> {
 	private inline function set_two(v : T):T {
 		return (this[1] = v);
 	}
+	
+	/**
+	  * Alias to "two"
+	  */
+	public var max(get, set) : T;
+	private inline function get_max():T {
+		return (this[1]);
+	}
+	private inline function set_max(v : T):T {
+		return (this[1] = v);
+	}
 
 /* === Instance Methods === */
+	
 	@:to
 	public inline function toString():String {
 		return 'Range($one...$two)';
 	}
 
+/* === Static Instance Methods === */
+
+	/**
+	  * Iterate over a Range of Integers
+	  */
+	public static inline function iterator(r : Range<Int>):Iterator<Int> {
+		return (r.min...r.max);
+	}
+
+	/**
+	  * Select random value from Range of Integers
+	  */
+	public static function rand(me:Range<Int>, ?r:Random):Int {
+		r = (r != null ? r : (new Random()));
+
+		return r.randint(me.min, me.max);
+	}
 	
 /* === Operator Methods === */
 	
